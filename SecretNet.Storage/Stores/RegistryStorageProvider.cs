13945247718dd2577a5a5feaff853e6f63412684
Serdar Demir <serdardemir@outlook.com>
2019-06-10 00:32:17 +0300

    RegistryStorageProvider added

---
+++ SecretNet.Storage/Stores/RegistryStorageProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SecretNet.Storage.Stores
+{
+    public class RegistryStorageProvider : StorageProvider
+    {
+        protected override void InternalCreate(string key, object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InternalDelete(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object InternalGet(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InternalUpdate(string key, object data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
